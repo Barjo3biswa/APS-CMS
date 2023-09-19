ab6a0d9fb55e6bc1815a83312fae8d0fcc35a667
Biswajit Laskar <biswajit2013laskar@gmail.com>
2023-09-19 18:11:11 +0530

    test

--- resources/views/content/page.blade.php
+++ resources/views/content/page.blade.php
@@ -48,6 +48,7 @@
                         </ol>
                     </nav>
                 </div>
+
                 <img src="{{asset($content->banner_image??"new-images/slider3.jpg")}}" alt="Banner Image">
             </div>
         </div>
